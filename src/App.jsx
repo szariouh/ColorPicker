c22e9fdc32526c67829cf654f6831a36c7a63ea6
Soumya Zariouh <soumya.zariouh@gmail.com>
2024-02-17 17:21:23 -0600

    added picking colors feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-// Write your Color component here
+import {useState} from 'react'
 
+const Color = ({color, selectedColor, setSelectedColor}) => {        // using object deconstruction
+  return (
+    <div className={color===selectedColor? color + ' selected' : color}
+    onClick={()=>setSelectedColor(color)}
+    ></div>
+  )
+}
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("skyblue")
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="orange" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="skyblue" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+        <Color color="green" selectedColor={selectedColor} setSelectedColor={setSelectedColor} />
+      </div>
     </div>
   );
 };
